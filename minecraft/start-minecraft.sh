cfec26e4c4dd4e77b7c33f11c3aeb39f8cf0e4fc
Tim Sheahan <tim@timsheahan.com>
2026-01-04 11:43:00 +1100

    Refined minecraft start-stop scripts use contemporary logging flag and use an ipv4 flag to prevent loss of connectivity by v6 preemption on the listen port.

--- minecraft/start-minecraft.sh
+++ minecraft/start-minecraft.sh
@@ -1,28 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ ! -e server.properties ]; then
   echo "server.properties not found, aborting."
   exit 1
 fi
 
-# Flag indicating if the installed version of screen is < 4.06
-# that does not support the -Logfile argument.
-# Set legacyScreen=1 for true, and legacyScreen=0 for false.
-legacyScreen=1
+# numeric boolean flag to work around screen < 4.06 lacking -Logfile argument
+# legacyScreen=0  # (handler omitted in this commit)
 
 logFile="console_$(date +"%Y-%m-%d_%H-%M-%S").log"
-# TODO:
-#   - investigate how closely this mirrors the minecraft logs in /logs
-#   - verify the stop workflow vs instance shutdown - does the minecraft server stop properly?
 
 echo "logfile is $logFile"
 
-if [ "$legacyScreen" -eq 1 ]; then
-  cat << EOF >/tmp/screenrc.$$
-logfile $logFile
-EOF
-  /usr/bin/screen -DmS minecraft -L -c /tmp/screenrc.$$ java -Xmx4092M -Xms4092M -jar minecraft_server.jar
-  rm /tmp/screenrc.$$
-else
-  /usr/bin/screen -DmS minecraft -L -Logfile "$logFile" java -Xmx4092M -Xms4092M -jar minecraft_server.jar
-fi
+/usr/bin/screen -DmS minecraft -L -Logfile "$logFile" java -Xmx4092M -Xms4092M -Djava.net.preferIPv4Stack=true -jar minecraft_server.jar
--- minecraft/stop-minecraft.sh
+++ minecraft/stop-minecraft.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 /usr/bin/screen -S minecraft -p 0 -X stuff "/say systemd is shutting down this service.^M"
 sleep 5
 /usr/bin/screen -S minecraft -p 0 -X stuff "/stop^M"
